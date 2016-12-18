85792459f281888252c2f62ddcc9d56bf4987956
Aaron <Aaron@DESKTOP-O1AMTMI>
2016-12-18 13:45:19 +0000

    Windsor Installer so far (not working)

--- WebGameBacklog/Global.asax.cs
+++ WebGameBacklog/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using FluentNHibernate.Cfg;
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
+using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using NHibernate.Tool.hbm2ddl;
 using System;
@@ -8,12 +10,14 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebGameBacklog.Infrastructure;
 using WebGameBacklog.Persistence.Mappings;
 
 namespace WebGameBacklog
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private WindsorContainer _windsorContainer;
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -21,11 +25,30 @@ namespace WebGameBacklog
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            var sessionFactory = Fluently.Configure()
-                .Database()
-                .Mappings(x => x.FluentMappings.AddFromAssemblyOf<GameMap>())
-                .ExposeConfiguration(cfg => new SchemaExport(cfg).Execute(true, true, false))
-                .BuildSessionFactory;
+            InitializeWindsor();
+
+            //var sessionFactory = Fluently.Configure()
+            //    .Database()
+            //    .Mappings(x => x.FluentMappings.AddFromAssemblyOf<GameMap>())
+            //    .ExposeConfiguration(cfg => new SchemaExport(cfg).Execute(true, true, false))
+            //    .BuildSessionFactory;
+        }
+
+        protected void Application_End()
+        {
+            if (_windsorContainer != null)
+            {
+                _windsorContainer.Dispose();
+            }
+        }
+
+        private void InitializeWindsor()
+        {
+            _windsorContainer = new WindsorContainer();
+            _windsorContainer.Install(FromAssembly.This());
+
+            ControllerBuilder.Current.SetControllerFactory(
+                new WindsorControllerFactory(_windsorContainer.Kernel));
         }
     }
 }
---
+++ WebGameBacklog/Infrastructure/DependencyInstaller.cs
@@ -0,0 +1,75 @@
+﻿using Castle.MicroKernel.Registration;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WebGameBacklog.Persistence.Mappings;
+
+namespace WebGameBacklog.Infrastructure
+{
+    public class DependencyInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Kernel.ComponentRegistered += Kernel_ComponentRegistered;
+
+            //Register all controllers
+            container.Register(
+
+                //Nhibernate session factory
+                Component.For<ISessionFactory>().UsingFactoryMethod(CreateNhSessionFactory).LifeStyle.Singleton,
+
+                //Unitofwork interceptor
+                Component.For<NhUnitOfWorkInterceptor>().LifeStyle.Transient,
+
+                //All repoistories
+                Classes.FromAssembly(Assembly.GetAssembly(typeof(NhPersonRepository))).InSameNamespaceAs<NhPersonRepository>().WithService.DefaultInterfaces().LifestyleTransient(),
+
+                //All services
+                Classes.FromAssembly(Assembly.GetAssembly(typeof(PersonService))).InSameNamespaceAs<PersonService>().WithService.DefaultInterfaces().LifestyleTransient(),
+
+                //All MVC controllers
+                Classes.FromThisAssembly().BasedOn<IController>().LifestyleTransient()
+
+                );
+        }
+
+        /// <summary>
+        /// Creates NHibernate Session Factory.
+        /// </summary>
+        /// <returns>NHibernate Session Factory</returns>
+        private static ISessionFactory CreateNhSessionFactory()
+        {
+            var connStr = ConfigurationManager.ConnectionStrings["PhoneBook"].ConnectionString;
+            return Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008.ConnectionString(connStr))
+                .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetAssembly(typeof(GameMap))))
+                .BuildSessionFactory();
+        }
+
+        void Kernel_ComponentRegistered(string key, Castle.MicroKernel.IHandler handler)
+        {
+            //Intercept all methods of all repositories.
+            if (UnitOfWorkHelper.IsRepositoryClass(handler.ComponentModel.Implementation))
+            {
+                handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(NhUnitOfWorkInterceptor)));
+            }
+
+            //Intercept all methods of classes those have at least one method that has UnitOfWork attribute.
+            foreach (var method in handler.ComponentModel.Implementation.GetMethods())
+            {
+                if (UnitOfWorkHelper.HasUnitOfWorkAttribute(method))
+                {
+                    handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(NhUnitOfWorkInterceptor)));
+                    return;
+                }
+            }
+        }
+    }
+}
