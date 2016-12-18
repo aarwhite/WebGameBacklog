5d4eb3461202f8016f92800ffa85b461e344ed73
Aaron <Aaron@DESKTOP-O1AMTMI>
2016-12-18 13:27:26 +0000

    Game Repo Added

---
+++ WebGameBacklog.Domain/Repositories/IGameRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebGameBacklog.Domain.Games;
+
+namespace WebGameBacklog.Domain.Repositories
+{
+    public interface IGameRepository : IRepository<Game>
+    {   
+    }
+}
---
+++ WebGameBacklog.Persistence/Repositories/GameRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebGameBacklog.Domain.Games;
+using WebGameBacklog.Domain.Repositories;
+
+namespace WebGameBacklog.Persistence.Repositories
+{
+    public class GameRepository : Repository<Game>, IGameRepository
+    {   
+    }
+}
