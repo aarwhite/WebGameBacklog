f846a7412c7f2eda92c39b3a02eb7020578f2d2e
aaronwhi <aaronwhi@live.com>
2017-01-16 21:51:17 +0000

    Fixed build issue

--- WebGameBacklog/Infrastructure/DependencyInstaller.cs
+++ WebGameBacklog/Infrastructure/DependencyInstaller.cs
@@ -10,6 +10,10 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using WebGameBacklog.Persistence.Mappings;
+using WebGameBacklog.Services;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System.Web.Mvc;
 
 namespace WebGameBacklog.Infrastructure
 {
@@ -17,22 +21,23 @@ namespace WebGameBacklog.Infrastructure
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            container.Kernel.ComponentRegistered += Kernel_ComponentRegistered;
-
             //Register all controllers
             container.Register(
 
                 //Nhibernate session factory
                 Component.For<ISessionFactory>().UsingFactoryMethod(CreateNhSessionFactory).LifeStyle.Singleton,
-
-                //Unitofwork interceptor
-                Component.For<NhUnitOfWorkInterceptor>().LifeStyle.Transient,
-
+                
                 //All repoistories
-                Classes.FromAssembly(Assembly.GetAssembly(typeof(NhPersonRepository))).InSameNamespaceAs<NhPersonRepository>().WithService.DefaultInterfaces().LifestyleTransient(),
+                Classes.FromAssembly(Assembly.GetAssembly(typeof(Persistence.Repositories.GameBacklogRepository)))
+                    .InSameNamespaceAs<Persistence.Repositories.GameBacklogRepository>()
+                    .WithService.DefaultInterfaces()
+                    .LifestyleTransient(),
 
                 //All services
-                Classes.FromAssembly(Assembly.GetAssembly(typeof(PersonService))).InSameNamespaceAs<PersonService>().WithService.DefaultInterfaces().LifestyleTransient(),
+                Classes.FromAssembly(Assembly.GetAssembly(typeof(Services.Games.GameBacklogService)))
+                    .InSameNamespaceAs<Services.Games.GameBacklogService>()
+                    .WithService.DefaultInterfaces()
+                    .LifestyleTransient(),
 
                 //All MVC controllers
                 Classes.FromThisAssembly().BasedOn<IController>().LifestyleTransient()
@@ -51,25 +56,6 @@ namespace WebGameBacklog.Infrastructure
                 .Database(MsSqlConfiguration.MsSql2008.ConnectionString(connStr))
                 .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetAssembly(typeof(GameMap))))
                 .BuildSessionFactory();
-        }
-
-        void Kernel_ComponentRegistered(string key, Castle.MicroKernel.IHandler handler)
-        {
-            //Intercept all methods of all repositories.
-            if (UnitOfWorkHelper.IsRepositoryClass(handler.ComponentModel.Implementation))
-            {
-                handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(NhUnitOfWorkInterceptor)));
-            }
-
-            //Intercept all methods of classes those have at least one method that has UnitOfWork attribute.
-            foreach (var method in handler.ComponentModel.Implementation.GetMethods())
-            {
-                if (UnitOfWorkHelper.HasUnitOfWorkAttribute(method))
-                {
-                    handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(NhUnitOfWorkInterceptor)));
-                    return;
-                }
-            }
         }
     }
 }
